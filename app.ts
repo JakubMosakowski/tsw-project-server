d3010bf2a1d3b89933d8ce94d73bd55778c91bec
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2019-05-21 11:04:49 +0200

    Added deletes

--- app.ts
+++ app.ts
@@ -21,93 +21,13 @@ server.listen(80);
 clearDB();
 authorize();
 setupHeaders();
+setupPosts();
+setupDeletes();
 
-app.post('/horse',
-    [
-        check('id').isNumeric(),
-        check('number').isNumeric(),
-        check('rank').isNumeric(),
-        check('yearOfBirth').isNumeric(),
-        check('color').isString(),
-        check('sex').isString(),
-        check('breeder.name').isString(),
-        check('breeder.country').isString(),
-        check('owner.name').isString(),
-        check('owner.country').isString(),
-        check('lineage.father.name').isString(),
-        check('lineage.father.country').isString(),
-        check('lineage.mother.name').isString(),
-        check('lineage.mother.country').isString(),
-        check('lineage.mothersFather.name').isString(),
-        check('lineage.mothersFather.country').isString(),
-        check('notes').isArray(),
-        check('notes.*.type').isNumeric(),
-        check('notes.*.head').isNumeric(),
-        check('notes.*.log').isNumeric(),
-        check('notes.*.legs').isNumeric(),
-        check('notes.*.movement').isNumeric(),
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({errors: errors.array()});
-        }
-
-        let horse = req.body;
-        horse.rankId = horse.rank;
-        delete horse.rank;
-
-        db.get(HORSES)
-            .push(horse)
-            .write();
-
-        res.json(horse);
-    });
-
-app.post('/judge',
-    [
-        check('id').isNumeric(),
-        check('name').isString(),
-        check('country').isString(),
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({errors: errors.array()});
-        }
-
-        db.get(JUDGES)
-            .push(req.body)
-            .write();
-
-        res.json(req.body);
-    });
-
-app.post('/rank',
-    [
-        check('id').isNumeric(),
-        check('number').isNumeric(),
-        check('category').isString(),
-        check('committee.*').isNumeric(),
-        check('committee').isArray(),
-
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({errors: errors.array()});
-        }
-
-        db.get(RANKS)
-            .push(req.body)
-            .write();
-
-        res.json(req.body);
-    });
-
-//todo  edycję i usuwanie danych o sędziach
-//todo  edycję i usuwanie danych o klasach
-//todo  edycję i usuwanie danych o koniach
+//todo  getters,edycję i  danych o sędziach
+//todo  getters,edycję i  danych o klasach
+//todo  getters,edycję i  danych o koniach
+//todo ustawianie id. Nowe id zastępuje stare i tyle (przy post sprawdzamy czy bylo takie id jezeli tak to usuwamy i dodajemy nowe w to miejsce).
 
 //todo możliwość przeniesienia konia z klasy do klasy
 //todo możliwość zmiany numerów startowych (np. „rozsunięcie” ich dla zrobienia miejsca na kolejnego konia)
@@ -124,8 +44,8 @@ function clearDB() {
     db.write();
     db.defaults({
         horses: [],
-        judges: {},
-        ranks: {}
+        judges: [],
+        ranks: []
     }).write();
 }
 
@@ -151,6 +71,132 @@ function setupHeaders() {
     });
 }
 
+function setupDeletes() {
+    app.delete('/horse/:id', function (req, res) {
+        let toRemove = db.get(HORSES).find({id: parseInt(req.params.id, 10)}).value();
+        db.get(HORSES)
+            .remove(toRemove)
+            .write();
+        if (toRemove == null) {
+            res.json(NOT_FOUND);
+        } else {
+            res.json();
+        }
+
+    });
+
+    app.delete('/judge/:id', function (req, res) {
+        let toRemove = db.get(JUDGES).find({id: parseInt(req.params.id, 10)}).value();
+        db.get(JUDGES)
+            .remove(toRemove)
+            .write();
+        if (toRemove == null) {
+            res.json(NOT_FOUND);
+        } else {
+            res.json();
+        }
+
+    });
+
+    app.delete('/rank/:id', function (req, res) {
+        let toRemove = db.get(RANKS).find({id: parseInt(req.params.id, 10)}).value();
+        db.get(RANKS)
+            .remove(toRemove)
+            .write();
+        if (toRemove == null) {
+            res.json(NOT_FOUND);
+        } else {
+            res.json();
+        }
+    });
+}
+
+function setupPosts() {
+    app.post('/horse',
+        [
+            check('id').isNumeric(),
+            check('number').isNumeric(),
+            check('rank').isNumeric(),
+            check('yearOfBirth').isNumeric(),
+            check('color').isString(),
+            check('sex').isString(),
+            check('breeder.name').isString(),
+            check('breeder.country').isString(),
+            check('owner.name').isString(),
+            check('owner.country').isString(),
+            check('lineage.father.name').isString(),
+            check('lineage.father.country').isString(),
+            check('lineage.mother.name').isString(),
+            check('lineage.mother.country').isString(),
+            check('lineage.mothersFather.name').isString(),
+            check('lineage.mothersFather.country').isString(),
+            check('notes').isArray(),
+            check('notes.*.type').isNumeric(),
+            check('notes.*.head').isNumeric(),
+            check('notes.*.log').isNumeric(),
+            check('notes.*.legs').isNumeric(),
+            check('notes.*.movement').isNumeric(),
+        ],
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.array()});
+            }
+
+            let horse = req.body;
+            horse.rankId = horse.rank;
+            delete horse.rank;
+
+            db.get(HORSES)
+                .push(horse)
+                .write();
+
+            res.json(horse);
+        });
+
+    app.post('/judge',
+        [
+            check('id').isNumeric(),
+            check('name').isString(),
+            check('country').isString(),
+        ],
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.array()});
+            }
+
+            db.get(JUDGES)
+                .push(req.body)
+                .write();
+
+            res.json(req.body);
+        });
+
+    app.post('/rank',
+        [
+            check('id').isNumeric(),
+            check('number').isNumeric(),
+            check('category').isString(),
+            check('committee.*').isNumeric(),
+            check('committee').isArray(),
+
+        ],
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({errors: errors.array()});
+            }
+
+            db.get(RANKS)
+                .push(req.body)
+                .write();
+
+            res.json(req.body);
+        });
+}
+
+const NOT_FOUND = {msg: "VALUE WAS NOT FOUND"};
 const HORSES = 'horses';
 const JUDGES = 'judges';
 const RANKS = 'ranks';
